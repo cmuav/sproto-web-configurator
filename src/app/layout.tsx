2d7bad56599e69081dfbedfc6241cb21fdcd4ff1
Camden Moors <camdenmoors@asi-uav.com>
2026-04-05 15:48:11 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,7 +49,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>⚡</text></svg>" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
